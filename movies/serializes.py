06b6c5cc897b83e41911426edc9b3c83b8c59828
Rennan Neves de Oliveira <rennan.n.oliveira@gmail.com>
2024-10-11 14:53:47 -0300

    feat: validation fields

--- movies/serializes.py
+++ movies/serializes.py
@@ -14,3 +14,13 @@ class MovieModelSerializer(serializers.ModelSerializer):
   class Meta:
     model = Movie
     fields = '__all__'
+  
+  def validate_release_date(self, value):
+    if value.year < 1990:
+      raise serializers.ValidationError('A data de lançamento não poder ser anterior a 1990')
+    return value
+  
+  def validate_sinopse(self, value):
+    if len(value) > 200:
+      raise serializers.ValidationError('A sinopse deve ter no máximo 200 caracteres')
+    return value
