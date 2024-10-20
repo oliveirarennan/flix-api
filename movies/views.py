f6ea194a7b862780b3cac7aa8b1d62267b432920
Rennan Oliveira <rennan.n.oliveira@gmail.com>
2024-10-19 22:04:00 -0300

    feat: add custom end point

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status
 from movies.models import Movie
 from movies.serializes import MovieModelSerializer, MovieSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -12,4 +12,14 @@ class MovieCreateListView(generics.ListCreateAPIView):
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
   permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
   queryset = Movie.objects.all()
-  serializer_class = MovieModelSerializer  
+  serializer_class = MovieModelSerializer
+
+class MovieStatsView(views.APIView):
+  permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+  queryset = Movie.objects.all()
+  def get(self, request):
+    return response.Response(
+      data={"message": "Funcionou!"},
+      status=status.HTTP_200_OK
+      
+    )
