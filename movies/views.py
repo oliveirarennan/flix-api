6ebca68423d9f0ef497b3fa59d9622911a435392
Rennan Neves de Oliveira <rennan.n.oliveira@gmail.com>
2024-10-21 15:48:21 -0300

    feat: add stats endpoint

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
+from django.db.models import Count, Avg
 from rest_framework import generics, views, response, status
 from movies.models import Movie
 from movies.serializes import MovieModelSerializer, MovieSerializer
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
+from reviews.models import Review
 
 class MovieCreateListView(generics.ListCreateAPIView):
   permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
@@ -18,8 +20,17 @@ class MovieStatsView(views.APIView):
   permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
   queryset = Movie.objects.all()
   def get(self, request):
+    total_movies = self.queryset.count()
+    movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+    total_reviews = Review.objects.count()
+    average_rating = Review.objects.aggregate(average_rating=Avg('rating'))['avg_rating']
+
     return response.Response(
-      data={"message": "Funcionou!"},
+      data={
+        'total_movies': total_movies,
+        'movies_by_genre': movies_by_genre,
+        'total_reviews': total_reviews,
+        'average_rating': round(average_rating, 1) if average_rating else 0
+      },
       status=status.HTTP_200_OK
-      
     )
